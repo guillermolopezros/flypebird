c1e92600d59ad58a47caa3c66ed43760655c3dd0
Guille <guillermolopezros@gmail.com>
2014-10-23 19:38:21 +0200

    menu
    menu

---
+++ Assets/Scripts/ScoreUp.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class ScoreUp : MonoBehaviour {
+
+
+	private Text texto;
+
+
+	void Start(){
+				texto = GameObject.Find ("TextoNivel").GetComponent<Text> ();
+
+		}
+	void OnTriggerEnter2D(Collider2D col) {
+		GameControl.score = GameControl.score + 1;
+
+
+		texto.text = "Level " + GameControl.score.ToString ();
+	
+	}
+
+}
